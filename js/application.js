721842bcc74cc5510c85715562a74da2287d2277
willowcheng <e.wingus@Gmail.com>
2015-12-12 20:24:55 -0500

    Update with refactoring

--- js/application.js
+++ js/application.js
@@ -4,19 +4,35 @@
 
 App.onLaunch = function(options) {
     // 1
-    var alert = createAlert("Hello World", ""); //leaving 2nd parameter with an empty string
-    navigationDocument.presentModal(alert);
+    var javascriptFiles = [
+        `${options.BASEURL}js/Presenter.js`
+    ];
+    // 2
+    evaluateScripts(javascriptFiles, function(success) {
+        if(success) {
+            var alert = createAlert("Welcome to Coala", "Find you nearby talent");
+            Presenter.modalDialogPresenter(alert);
+        } else {
+            // 3 Handle the error CHALLENGE!//inside else statement of evaluateScripts.
+            //inside else statement of evaluateScripts.
+            var errorDoc = createAlert("Evaluate Scripts Error", "Error attempting to evaluate external JavaScript files.");
+            navigationDocument.presentModal(errorDoc);
+        }
+    });
 };
 
-// 2
+
 var createAlert = function(title, description) {
     var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
-    <document>
+  <document>
     <alertTemplate>
-    <title>${title}</title>
-    <description>${description}</description>
+      <title>${title}</title>
+      <description>${description}</description>
+      <button>
+        <text>OK</text>
+      </button>
     </alertTemplate>
-    </document>`;
+  </document>`;
     var parser = new DOMParser();
     var alertDoc = parser.parseFromString(alertString, "application/xml");
     return alertDoc
